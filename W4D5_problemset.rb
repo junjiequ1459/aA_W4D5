70a114b20734b9f41c0ebd3ba9180a197ca1ea9c
“Shawna <shawna.e.hartley@gmail.com>
2022-12-02 12:08:18 -0500

    progress on sub_sum

--- W4D5_problemset.rb
+++ W4D5_problemset.rb
@@ -8,9 +8,24 @@ def my_min(arr)
   num
 end
 
-list = [0, 3, 5, 4, -5, 10, 1, 90]
+# list = [0, 3, 5, 4, -5, 10, 1, 90]
 # p my_min(list)
 
-def my_min_v2
-    
+def my_min_v2(arr)
+    min_num = arr[0]
+    arr.each {|ele| min_num = ele if min_num > ele}
+    min_num
+
 end
+
+# p my_min_v2(list)
+
+def sub_sum(arr)
+    new_arr = arr.filter {|ele| ele < 0}
+    result = new_arr.reduce {|c, v| c += v}
+    return result
+end
+
+list = [5, 3, -7]
+p sub_sum(list)
+
