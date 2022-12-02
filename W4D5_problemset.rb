7282675d83b48559c0564e15277496886b87d0d4
rexqu <junjiequ123@gmail.com>
2022-12-02 11:50:59 -0500

    fin my_min

--- W4D5_problemset.rb
+++ W4D5_problemset.rb
@@ -1,5 +1,12 @@
-def my_min
+def my_min(arr)
+  num = 0
+  i = 0
+  while i < arr.length
+    num = arr[i] if arr.all? { |ele| arr[i] <= ele }
+    i += 1
+  end
+  num
 end
 
-list = [0,3,5,4,-5,10,1,90]
-my_min(list)
+list = [0, 3, 5, 4, -5, 10, 1, 90]
+p my_min(list)
